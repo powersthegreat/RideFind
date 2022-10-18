d6eda99090e4c6df804864f6c872b6e30558231f
powersthegreat <williampowers@ku.edu>
2022-10-18 10:39:51 -0500

    10/18/2022 1

---
+++ src/pages/MainPage.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import classes from './MainPage.module.css';
+
+function MainPage() {
+    return (
+        <div className={classes.test}>
+            <p>This is the main page.</p>
+        </div>
+    );
+}
