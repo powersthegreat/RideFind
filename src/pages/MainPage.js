ebe5841fe9ca7956cd77318adfd0d268cc7a1bf9
powersthegreat <williampowers@ku.edu>
2022-10-18 10:47:53 -0500

    10/18/2022 2

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -4,8 +4,10 @@ import classes from './MainPage.module.css';
 
 function MainPage() {
     return (
-        <div className={classes.test}>
-            <p>This is the main page.</p>
-        </div>
+        <section className={classes.testsection}>
+            <h1 className={classes.testh1}>This is the main page.</h1>
+        </section>
     );
 }
+
+export default MainPage;
