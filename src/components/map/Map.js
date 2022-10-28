34386b5d7b735474476888ff06106875aaf70b45
Ehtisham Mushtaq <ehtishammushtaq@gmail.com>
2022-10-28 15:56:27 -0500

    First commit: Added google maps

--- src/components/map/Map.js
+++ src/components/map/Map.js
@@ -1,13 +1,26 @@
 import React from 'react';
-
+import { Map, GoogleApiWrapper } from 'google-maps-react';
 import classes from './Map.module.css';
 
-function Map() {
+function map() {
     return(
-        <div className={classes.container}>
-            <p>map</p>
-        </div>
+       <Map
+       google = {this.props.google}
+       style = {{width: "100%", height: "100%" }}
+       zoom = {10}
+       intialCenter = {
+        {
+            lat: 28.704060,
+            lng: 77.102493
+        }
+        }
+       
+    />
     );
 }
 
-export default Map;
+export default GoogleApiWrapper(
+    {
+        apiKey: "AIzaSyCYK8x8EyxKxEba0QftXmrSvC4TsDzlJg0"
+    }
+)(Map)
