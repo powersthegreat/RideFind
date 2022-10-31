dcb1394f46432a4b910d3dfd75a1d121f4e002e4
JazzyFierce <102490072+JazzyFierce@users.noreply.github.com>
2022-10-31 12:03:59 -0500

    Verify Map is centered

--- src/components/map/Map.js
+++ src/components/map/Map.js
@@ -3,6 +3,24 @@ import { Map, GoogleApiWrapper } from 'google-maps-react';
 import classes from './Map.module.css';
 
 function map() {
+    
+    // default center if no location services 
+    const pos = {
+        lat: 28.704060,
+        lng: 77.102493
+    }
+
+    // get current location if location services are enabled
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(
+          (position) => {
+            pos = {
+              lat: position.coords.latitude,
+              lng: position.coords.longitude
+            };
+        })
+    };
+    
     return(
        <Map
        google = {this.props.google}
@@ -10,8 +28,7 @@ function map() {
        zoom = {10}
        intialCenter = {
         {
-            lat: 28.704060,
-            lng: 77.102493
+            pos
         }
         }
        
