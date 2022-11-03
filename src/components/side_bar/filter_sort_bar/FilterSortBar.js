33fcbb177a4f9441b16cf6e6a482b7cbb805f62b
powersthegreat <williampowers@ku.edu>
2022-11-03 15:35:33 -0500

    building intial layout for filter sort bar

--- src/components/side_bar/filter_sort_bar/FilterSortBar.js
+++ src/components/side_bar/filter_sort_bar/FilterSortBar.js
@@ -6,7 +6,16 @@ import classes from './FilterSortBar.module.css';
 function FilterSortBar() {
     return (
         <div className={classes.container}>
-            <p>filter and sort bar here</p>
+            <div className={classes.leftdiv}>
+                <h3 className={classes.title}>Rides in Area</h3>
+                <p className={classes.location}>1111 Indiana St, Lawrence KS</p>
+            </div>
+            <div className={classes.rightdiv}>
+                <div className={classes.buttonsdiv}>
+                    <button className={classes.filterbutton} type="popup">Filter</button>
+                    <button className={classes.sortbutton} type="select">Sort</button>
+                </div>
+            </div>
         </div>
     );
 }
