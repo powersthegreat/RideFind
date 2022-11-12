aa11eae11d0d4504ed88f6f49591f6196edbc614
Ehtisham Mushtaq <ehtishammushtaq@gmail.com>
2022-11-12 12:40:16 -0600

    Added top logo, and nav buttons

--- src/components/layout/MainNavigation.js
+++ src/components/layout/MainNavigation.js
@@ -1,24 +1,38 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
 import classes from './MainNavigation.module.css'
+import MainLogo from '../side_bar/logos/mainLogo.png';
+import HomeLogo from '../side_bar/logos/homeLogo.png';
+import TutorialLogo from '../side_bar/logos/tutorialLogo.png';
+import ResourceLogo from '../side_bar/logos/resourcesLogo.png';
 
 
 function MainNavigation() {
 
     return (
         <header className={classes.header}>
-            <div>RideFind</div>
-            <nav>
+            <div className='mainLogo'>
+                <Link to="/">
+                    <img style={{ width: 300, height: 80, position:'relative'}} src={MainLogo} alt="mainLogo"/>
+                </Link>
+                
+            </div>
+            <nav className={classes.header}>
                 <ul>
                     <li>
-                        <Link to="/">Home</Link>
+                        <Link to="/">
+                            <img style={{ width: 120, height: 48,position:'relative' }} src={HomeLogo} alt="homeLogo"/>
+                        </Link>
                     </li>
                     <li>
-                        <Link to="/tutorial">Tutorial</Link>
+                        <Link to="/tutorial">
+                            <img style={{ width: 120, height: 48,position:'relative' }} src={TutorialLogo} alt="homeLogo"/>
+                        </Link>
                     </li>
                     <li>
-                        <Link to="/resources">Resources</Link>
+                        <Link to="/resources">
+                            <img style={{ width: 120, height: 48,position:'relative'}} src={ResourceLogo} alt="homeLogo"/>
+                        </Link>
                     </li>
                 </ul>
             </nav>
