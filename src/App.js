5bbe08af1fe61c6f2f2844524e82555c889d67c2
Ehtisham Mushtaq <ehtishammushtaq@gmail.com>
2022-11-12 19:37:28 -0600

    Added Tutorial page

--- src/App.js
+++ src/App.js
@@ -4,18 +4,20 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import MainPage from './pages/MainPage'
 import ErrorPage from './pages/ErrorPage';
 import Layout from './components/layout/Layout';
-import ResourcesPage from './pages/ResourcesPage';
-import TutorialPage from './pages/TutorialPage';
+import ResourcesPage from './pages/resourcePage/ResourcesPage';
+import TutorialPage from './pages/tutorialPage/TutorialPage';
 
 function App() {
   return (
     <BrowserRouter>
       <Layout>
         <Routes>
-          <Route path="/" element={<MainPage></MainPage>} />
-          <Route path="/tutorial" element={<TutorialPage></TutorialPage>} />
-          <Route path="/resources" element={<ResourcesPage></ResourcesPage>} />
-          <Route path="*" element={<ErrorPage></ErrorPage>} />
+          
+            <Route path="/" element={<MainPage></MainPage>} />
+            <Route path="/tutorial" element={<TutorialPage></TutorialPage>} />
+            <Route path="/resources" element={<ResourcesPage></ResourcesPage>} />
+            <Route path="*" element={<ErrorPage></ErrorPage>} />
+         
         </Routes>
       </Layout>
     </BrowserRouter>
---
+++ src/pages/tutorialPage/components/Hero.js
@@ -0,0 +1,67 @@
+import {
+  Box,
+  Button,
+  Flex,
+  Img,
+  Spacer,
+  Text,
+  useMediaQuery,
+} from '@chakra-ui/react';
+import React from 'react';
+import chakraHero from '../assets/chakraHero.jpg';
+
+const Hero = () => {
+  const [isLargerThanLG] = useMediaQuery('(min-width: 62em)');
+  return (
+    <Flex
+      alignItems="center"
+      w="full"
+      px={isLargerThanLG ? '16' : '6'}
+      py="16"
+      minHeight="90vh"
+      justifyContent="space-between"
+      flexDirection={isLargerThanLG ? 'row' : 'column'}
+    >
+      <Box mr={isLargerThanLG ? '6' : '0'} w={isLargerThanLG ? '60%' : 'full'}>
+        <Text
+          fontSize={isLargerThanLG ? '5xl' : '4xl'}
+          fontWeight="bold"
+          mb="4"
+        >
+          {' '}
+          Tutorial Page
+        </Text>
+
+        <Text mb="6" fontSize={isLargerThanLG ? 'lg' : 'base'} opacity={0.7}>
+          Sample Code for the blog article React MUI Components - Learn by Coding. The article explains how to code from scratch all components: 
+          navBar, hero section, app features, contact, and footer. 
+        </Text>
+
+        <Button
+          w="200px"
+          colorScheme="blue"
+          variant="solid"
+          h="50px"
+          size={isLargerThanLG ? 'lg' : 'md'}
+          mb={isLargerThanLG ? '0' : '10'}
+          onClick={(e) => {
+            e.preventDefault();
+            window.location.href='https://blog.appseed.us/chakra-ui-react-coding-landing-page/';
+            }}
+        >
+          Read More
+        </Button>
+      </Box>
+      <Spacer />
+      <Flex
+        w={isLargerThanLG ? '40%' : 'full'}
+        alignItems="center"
+        justifyContent="center"
+      >
+        <Img src={chakraHero} alt="Chakra UI" />
+      </Flex>
+    </Flex>
+  );
+};
+
+export default Hero;
